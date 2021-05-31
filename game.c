c947f141cfe60865690ec60826e06f4b133a6de7
Mahdee Mohammad <maxdeexd@yahoo.com>
2021-05-31 13:25:21 +0600

    Add files via upload

--- game.c
+++ game.c
@@ -16,58 +16,60 @@ int main(int argc, char **argv){
     while(ch != '1'){
 	system("$PREFIX/bin/stty cooked;clear;");
 	printf("\t     -----Move for %c-----\n", starter);
-        if(ch == 'k'){
-            x = x -4;
-            if(x < 3){
-                x = 11;
-            }
-        }
-        else if(ch == 'j'){
-            y = y - 4;
-            if(y < 2){
-                y = 10;
-            }
-        }
-        else if(ch == 'm'){
-            x = x+4;
-            if(x > 12){
-                x = 3;
-            }
-        }
-        else if(ch == 'l'){
-            y = y+4;
-            if(y > 12){
-                y=2;
-            }
-        }
-	else if(ch == ' ' && count <= 9){
-            if(1 == dif(x, y)){
-	        printf("You cannot place your move here.\n");
-            }
-            else{
-		moves[count] = starter;
-                showx[count] = x;
-                showy[count] = y;
-                count++;
-		starter = (starter == 'X')?'O':'X';
-		short int tmp = win();
-                if(1 == tmp){
-                    printf("Player %c has won!\n", moves[0]);
-		    system("$PREFIX/bin/stty cooked");
-		    return 0;
-                }
-		else if(2 == tmp){
-			printf("Player %c has won!\n", moves[1]);
-			system("$PREFIX/bin/stty cooked");
-			return 0;
-		}
-		else if(3 == tmp){
-			printf("Tie! Press 1 to exit.\n");
-		}
+        switch(ch){
+                case 'k':
+                        x = x - 4;
+                        if(x < 3){
+                                x = 11;
+                        }
+                        break;
+                case 'j':
+                        y = y - 4;
+                        if(y < 2){                                                                    y = 10;
+                        }
+                        break;
+                case 'm':
+                        x = x + 4;
+                        if(x > 12){
+                                x = 3;
+                        }
+                        break;
+                case 'l':
+                        y = y +4;
+                        if(y > 12){
+                                y = 2;
+                        }
+                        break;
+                case ' ':
+                        if(1 == dif(x, y)){
+                                printf("You cannot place your move here.\n");
+                        }
+                        else{
+                                moves[count] = starter;
+                                showx[count] = x;
+                                showy[count] = y;
+                                count++;
+                                starter = (starter == 'X')?'O':'X';
+                                short int tmp = win();
+                                if(1 == tmp){
+                                        printf("Player %c has won!\n", moves[0]);
+                                        system("$PREFIX/bin/stty cooked");
+                                        return 0;
+                                }
+                                else if(2 == tmp){
+                                        printf("Player %c has won!\n", moves[1]);
+                                        system("$PREFIX/bin/stty cooked");
+                                        return 0;
+                                }
+                                else if(3 == tmp){
+                                        printf("Tie! Press 1 to exit.\n");
+                                }
 
-            }
+                        }
+                        break;
+                default:
+                        help();
         }
-	else{ help(); };
         for(i=1;i<=13;i++){
             printf("\t\t\33[1;35m!\33[0m");
             for(j=1;j<=12;j++){
